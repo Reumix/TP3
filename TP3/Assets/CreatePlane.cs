805137f4e94d50b315670ed5f687d2aa147cac95
Reumiii <Remi.CHABRERIE@etu.uca.fr>
2023-03-13 09:27:40 +0100

    CreatePlane fonctionnel

--- TP3/Assets/CreatePlane.cs
+++ TP3/Assets/CreatePlane.cs
@@ -16,29 +16,52 @@ public class CreatePlane : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        nb_vertices = (resolution + 1) * (resolution + 1);
+        nb_triangles = resolution * resolution * 2;
+
         p_mesh = new Mesh();
-        //nb_vertices = resolution * resolution;
-        //nb_triangles = 2 * (resolution - 1) * (resolution - 1);
-        //p_vertices = new Vector3[nb_vertices];
-        //p_triangles = new int[nb_triangles];
-
-        nb_vertices = 4;
-        nb_triangles = 2;
-        p_vertices = new Vector3[nb_vertices];
-
-        p_vertices[0] = new Vector3(0, 0, 0);
-        p_vertices[1] = new Vector3(0, 0, 1);
-        p_vertices[2] = new Vector3(1, 0, 1);
-        p_vertices[3] = new Vector3(1, 0, 0);
-
-        p_triangles = new int[]
-        {
-            0,1,2,
-            0,2,3
-        };
-
-        p_mesh.vertices = p_vertices;
-        p_mesh.triangles = p_triangles;
         GetComponent<MeshFilter>().mesh = p_mesh;
+
+        p_vertices = new Vector3[nb_vertices];
+        p_triangles = new int[nb_triangles * 3];
+
+        float step = (float)dimension / resolution;
+
+        int vertexIndex = 0;
+        for (int z = 0; z <= resolution; z++)
+        {
+            for (int x = 0; x <= resolution; x++)
+            {
+                float xPos = x * step - dimension / 2f;
+                float zPos = z * step - dimension / 2f;
+                p_vertices[vertexIndex] = new Vector3(xPos, 0, zPos);
+                vertexIndex++;
+            }
+        }
+        p_mesh.vertices = p_vertices;
+       
+        int triangleIndex = 0;
+        for (int z = 0; z < resolution; z++)
+        {
+            for (int x = 0; x < resolution; x++)
+            {
+                int vertex = z * (resolution + 1) + x;
+
+                p_triangles[triangleIndex] = vertex + 1;
+                p_triangles[triangleIndex + 1] = vertex;
+                p_triangles[triangleIndex + 2] = vertex + resolution + 2;
+
+                p_triangles[triangleIndex + 3] = vertex;
+                p_triangles[triangleIndex + 4] = vertex + resolution + 1;
+                p_triangles[triangleIndex + 5] = vertex + resolution + 2;
+
+                triangleIndex += 6;
+            }
+        }
+        p_mesh.triangles = p_triangles;
+
+        p_mesh.RecalculateBounds();
+        p_mesh.RecalculateNormals();
+
     }
 }
